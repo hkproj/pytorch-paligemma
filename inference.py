73cab04744c47cd02b37f9d9e883086bc8ec3588
Umar Jamil <umar@writer.com>
2024-07-13 17:42:38 +0200

    removed unused imports

--- inference.py
+++ inference.py
@@ -1,6 +1,5 @@
 from PIL import Image
 import torch
-import json
 import fire
 
 from processing_paligemma import PaliGemmaProcessor
--- modeling_gemma.py
+++ modeling_gemma.py
@@ -1,6 +1,6 @@
 import torch
 from torch import nn
-from typing import Optional, Tuple, List, Dict, Any
+from typing import Optional, Tuple, List
 from torch.nn import CrossEntropyLoss
 import math
 from modeling_siglip import SiglipVisionConfig, SiglipVisionModel
