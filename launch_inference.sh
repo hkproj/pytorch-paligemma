379d1145c9cb0898d71126ee67e183fbd0b27c73
Umar Jamil <umar@writer.com>
2024-07-14 08:49:55 +0200

    fixed params for script

--- launch_inference.sh
+++ launch_inference.sh
@@ -10,9 +10,9 @@ DO_SAMPLE="False"
 ONLY_CPU="False"
 
 python inference.py \
-    --model_path $MODEL_PATH \
-    --prompt "\"$PROMPT\"" \
-    --image_file_path $IMAGE_FILE_PATH \
+    --model_path "$MODEL_PATH" \
+    --prompt "$PROMPT" \
+    --image_file_path "$IMAGE_FILE_PATH" \
     --max_tokens_to_generate $MAX_TOKENS_TO_GENERATE \
     --temperature $TEMPERATURE \
     --top_p $TOP_P \
